b95f60749e81d25f63d9f3fb0b1b61d0ae233d5f
ADM Moe janmohammad <admmjanmohammad@gdt-labs.local>
2017-03-10 09:36:14 -0600

    Initial commit
    create file

---
+++ sumprimes.py
@@ -0,0 +1,12 @@
+def primes_sieve(limit):
+    primes = []
+    limitn = limit + 1
+    not_prime = [False] * limitn
+    for i in range (2, limitn):
+        if not_prime[i]:
+            continue
+        for j in xrange(i*2, limitn, i):
+            not_prime[j] = True
+        primes.append(i)
+    return primes
+print(sum(primes_sieve(110)))
